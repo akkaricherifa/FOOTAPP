7149971dff3a208d5e63651d6c7c09aa2b9233a0
cherifa.informatique@gmail.com <cherifa.informatique@gmail.com>
2025-05-06 11:45:01 +0100

    updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DetailsComponent } from './details/details.component';
 import { AccueilComponent } from './accueil/accueil.component';
+import { FormulaireComponent } from './formulaire/formulaire.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'team/:id', component: DetailsComponent },
+    { path: 'formAJout',component: FormulaireComponent},
     { path: 'accueil', component: AccueilComponent },
     { path: '', redirectTo: '/accueil', pathMatch: 'full' } // Redirection vers 'accueil' par défaut
 ];
